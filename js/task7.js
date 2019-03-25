1ebea171668c90f9e3eaa507651daeeee7d2e6af
“Yulya <yeellowfish@gmail.com>
2019-03-25 15:38:27 +0800

    #add task 7

--- js/task7.js
+++ js/task7.js
@@ -1,14 +1,16 @@
-//Поиск целых положительных чисел, неравных нулю
-// @param incomingString (string) - строка, из которой нужно вырезать числа
-
-function findEvenPositiveNumbers (incomingString) {
-	var arr = incomingString.match(/(?<![-\d])(?<!\d[.,])(?!0)(\d*[24680])(?![.,]?\d)/g);
-	return String(arr);
-}
-
 document.getElementById('check').onclick = function() {
-	document.getElementById('result').innerHTML = '';
-	var newElem = document.createElement('p');
-	newElem.innerHTML = findEvenPositiveNumbers(document.getElementById('input').value);
-	document.getElementById('result').appendChild(newElem);
+	alert(calculateFactorial(document.getElementById('input').value));
 };
+
+// Вычисление факториала числа
+// @param incomingString (string) - строка, из которой нужно вырезать число, факториал которого мы высчитываем
+
+function calculateFactorial (incomingString) {
+  if (incomingString) {
+  	return incomingString * calculateFactorial(incomingString - 1);
+  }
+
+  else {
+  	return 1;
+  }
+}
