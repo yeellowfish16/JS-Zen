a669e3e5b73a6af4e779163d2cb9a179bf607571
“Yulya <yeellowfish@gmail.com>
2019-04-17 09:48:16 +0800

    #fix task 2

--- js/task2.js
+++ js/task2.js
@@ -1,10 +1,10 @@
 document.getElementById('check').onclick = function() {
-	calculateSecInHour(1);
+	calculateSecInHour(alert(calculateSecInHour(1)));
 };
 
 // Функция, считающая количество секунд в часе
 // @param numHours - количество часов, в которых нужно посчитать секунды
 
 function calculateSecInHour (numHours) {
-	alert('В ' + numHours + ' часе ' + 60*60*numHours + ' секунд');
+	return 'В ' + numHours + ' часе ' + 60*60*numHours + ' секунд';
 }
