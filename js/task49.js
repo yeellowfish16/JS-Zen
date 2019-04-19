e6ff82def73a3035ebfab06a7641ddafecbda5ba
“Yulya <yeellowfish@gmail.com>
2019-04-19 09:35:06 +0800

    #add task49

---
+++ js/task49.js
@@ -0,0 +1,19 @@
+document.getElementById('check').onclick = function () {
+    var newElem = document.createElement('p');
+    document.getElementById('result').innerHTML = '';
+    newElem.innerHTML = checkStartOfString(document.getElementById('input').value, 'http://');
+    document.getElementById('result').appendChild(newElem);
+};
+
+// Функция, проверяющая совпадение начала строки с шаблоном
+// @param string - строка, которую проверяем на соответствие
+// @param strTesmplate - шаблон, с которым сравниваем
+
+function checkStartOfString(string, strTemplate) {
+    if (string.substr(0, strTemplate.length) === strTemplate) {
+        return 'Все верно'
+    }
+    else {
+        return 'Нет, строка начинается с чего-то другого'
+    }
+}
