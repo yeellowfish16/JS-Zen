e7c448097fb59c736c08fedd4be0e936556553b8
“Yulya <yeellowfish@gmail.com>
2019-04-22 11:54:46 +0800

    #add task62

---
+++ js/task62.js
@@ -0,0 +1,23 @@
+var arr = [[1, 2, 3], [4, 5], [6]];
+
+document.getElementById('check').onclick = function () {
+    var newElem = document.createElement('div');
+    document.getElementById('result').innerHTML = '';
+    newElem.innerHTML = sumArrayElements(arr);
+    document.getElementById('result').appendChild(newElem);
+};
+
+function sumArrayElements(array) {
+    var newArray = [],
+        result = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        newArray = array[i];
+
+        for (var j = 0; j < newArray.length; j++) {
+            result += newArray[j];
+        }
+    }
+
+    return result;
+}
