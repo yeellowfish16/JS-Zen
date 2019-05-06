163f998b551b41bffb0ce8e6d0aee415e76f580a
“Yulya <yeellowfish@gmail.com>
2019-05-06 11:44:16 +0800

    #add task85

---
+++ js/task85.js
@@ -0,0 +1,18 @@
+var arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+day = 0;
+
+document.getElementById('check').onclick = function () {
+    showDays(arr);
+};
+
+// Функция, чтобы вывести ключ объекта и его свойство
+
+function showDays(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (i == day) {
+            document.write('<i>' + arr[i] + '</i><br>');
+        } else {
+            document.write(arr[i] + '<br>');
+        }
+    }
+}
